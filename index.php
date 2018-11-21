2db82bb0150eda747007b98004ac8b40cd072aaf
Richrao22 <43731939+Richrao22@users.noreply.github.com>
2018-11-21 10:07:08 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!-- ***************************************************************************************
+    Author     : Richard Orfao 
+    Course     :  CGS 4854-U01 (82024) Websiteconmgmt Class Lecture MoWe    5:00PM  -  6:15PM @ECS 136
+    URL        : ocelot.aul.fiu.edu/~rorfa001
+    Professor  : Michael Robinson 
+    Program #  : Assignment Purpose/Description 
+                 { To work with tables, fonts, colors and images } 
+
+    Due Date   : 09/05/2018 
+    Certification: 
+    I hereby certify that this work is my own and none of it is the work of any other person. 
+
+    ..........{ Richard Orfao }..........
+
+  **************************************************************************************** -->
+
+<html>
+  <!-- Head and Title goes here -->
+  <head>
+
+    <title>Orfao Richard Home</title>
+    
+  </head>
+
+
+  <!-- I have the FIU website and the with the programs -->
+  <body link="blue" vlink="blue" alink="red">
+
+    <!-- Table For Top Part FIU EDUCATIONAL -->
+    <table width="100%">
+      <tr>
+        <td align="center">
+          <font size="5" color="#FF1A0A" face="Helvetica"> <b> ~~~~ This is an educational website ~~~~ </b> </font>
+          <br>
+          <font size="5" color="#0736EE" face="Helvetica"> <b> <a href="https://www.cis.fiu.edu/">Florida International University</a> </b> </font>
+        <br>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <br>
+          <font size="3" face="Arial" color="#227C9C">Welcome to Orfao Richard Website!</font>
+          <br>
+        </td>
+      </tr>  
+    </table>
+
+    <hr>
+
+    <!-- Table to insert the programs links -->
+    <table width=100%" align="Center">
+      
+      <tr align="Center" >
+
+        <td align="center">
+          <font size="5" color=""> PROGRAMS  </font>
+        </td>
+
+      </tr>
+    </table>
+
+    <?php include('includes/mainMenu.php') ?>
+
+  </body>
+</html>
